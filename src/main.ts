ffe61486f504e20efa38bfedec8f36a155719e55
caitl <wils1353@purdue.edu>
2025-02-22 02:57:40 -0500

    RIP Cat Facts My Beloved

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { DataFetcherComponent } from './app/data-fetcher/data-fetcher.component';
 import { HomePageComponent } from './app/home-page/home-page.component';
 import { CompanyPageComponent } from './app/company-page/company-page.component';
 import { LeaveAReviewComponent } from './app/leave-areview/leave-areview.component';
@@ -13,7 +12,6 @@ bootstrapApplication(AppComponent, {
       { path: 'home', component: HomePageComponent },
       { path: 'company/:name', component: CompanyPageComponent },
       { path: 'review', component: LeaveAReviewComponent },
-      { path: 'test', component: DataFetcherComponent },
     ])
   ]
 }).catch((err) => console.error(err));
